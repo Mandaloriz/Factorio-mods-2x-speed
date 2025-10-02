9b7a06a1dc59a70cdef51a5dc8341b41ee5f649c
Mandaloriz <ltypopov@gmail.com>
2025-10-02 18:19:03 +0700

    add

---
+++ double_running_speed_0.1.0/data.lua
@@ -0,0 +1,17 @@
+--<<<<<<< HEAD:double_running_speed_0.1.0/cr/data.lua
+-- data.lua
+
+-- Изменяем свойства персонажа
+--data.raw["character"]["character"].running_speed = 2.0 -- Базовая скорость = 1.0
+-- kaka
+--=======
+-- data.lua
+
+-- Изменяем свойства персонажа
+--data.raw["character"]["character"].running_speed = 2.0 -- Базовая скорость = 1.0
+-- kaka
+
+--lua
+--require("prototypes.items")
+--require("prototypes.entities")
+-->>>>>>> 1b6d6c835831e3ff97c32a6aed9629ef7cf17b8d:double_running_speed_0.1.0/data.lua
