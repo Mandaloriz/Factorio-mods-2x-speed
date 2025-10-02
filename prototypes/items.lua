9818ee8d1685f15b32c168274eeb606d7ce43473
Mhofff <lenaaleksandrovic82@gmail.com>
2025-10-02 18:11:16 +0700

    rewwerrew

---
+++ prototypes/items.lua
@@ -0,0 +1,12 @@
+data:extend({
+  {
+    type = "item",
+    name = "decorative-item",
+    icon = "__your-mod-name__/graphics/icons/decorative-item.png",
+    icon_size = 64,
+    subgroup = "terrain",
+    order = "b[decorative]",
+    stack_size = 50,
+    place_result = "decorative-entity"
+  }
+})
