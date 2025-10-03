fe6499d1cb0cb98b5e5db9edf6b9daef1f65af6f
Mhofff <lenaaleksandrovic82@gmail.com>
2025-10-03 23:10:30 +0700

    fix
    locale, new settings

--- fire-armor_0.1.0/settings.lua
+++ fire-armor_0.1.0/settings.lua
@@ -4,5 +4,11 @@ data:extend({
         name = "my-mod-test-setting",
         setting_type = "runtime-global",
         default_value = true
+    },
+    {
+        type = "int-setting",
+        name = "numset",
+        setting_type = "startup",
+        default_value = 1
     }
 })
